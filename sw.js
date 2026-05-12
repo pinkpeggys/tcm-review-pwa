c4c58885b1fd11953cfab33dc4aa849f962fc16f
pinkpeggys <pinkpeggys@gmail.com>
2026-05-13 00:10:55 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "tcm-review-pwa-v4";
+const CACHE_NAME = "tcm-review-pwa-v6";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=4",
-  "./app.js?v=4",
+  "./styles.css?v=6",
+  "./app.js?v=6",
   "./manifest.webmanifest",
   "./icons/icon.svg",
 ];
